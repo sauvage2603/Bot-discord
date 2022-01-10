a9bd92adf744b5371207e3c1e4a0b7ec6fbfb522
Emmerick Fontaine <Emmerick.Fontaine@gmail.com>
2022-01-10 11:11:44 +0100

    bASE 1

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, Intents } = require("discord.js");
+require("dotenv").config();
+
+const botToken = process.env.TOKEN;
+
+console.log(botToken);
+
+// // Créer une instance de mon bot
+// const client = new Client({
+//     intents: [Intents.FLAGS.GUILDS]
+// });
+
+// // Une fois que mon bot est "ready" (en ligne)
+// client.once("ready", () => {
+//     console.log("Okay. Mon bot est en ligne!");
+// });
+
+// // Permet de lier notre bot à notre serveur
+// client.login(token);
